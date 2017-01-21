40f67f60e5bf13a163b6128b7484b6d2daa5d8d7
Li Kai <falconets25601@gmail.com>
2017-01-21 13:20:14 +0800

    Add vue router

--- src/main.js
+++ src/main.js
@@ -1,16 +1,32 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRouter from 'vue-router'
+
 import App from './App';
 import ghGot from 'gh-got';
 import store from './vuex/store';
 
+Vue.use(VueRouter)
 Vue.prototype.$http = ghGot;
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for routes: routes
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   store,
+  router,
   template: '<App/>',
   components: { App },
 });
