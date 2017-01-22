7b4d4e7ad79ab6a7683da20e8f6194b3af01c966
Li Kai <falconets25601@gmail.com>
2017-01-22 08:40:19 +0800

    Commit front

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,144 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-    <div class="row">
-      <div class="small-2 large-4 columns"><!-- ... --></div>
-      <div class="small-4 large-4 columns"><!-- ... --></div>
-      <div class="small-6 large-4 columns"><!-- ... --></div>
-    </div>
+<transition appear>
+  <div id="app" class="hero">
+    <section>
+      <div class="row header">
+        <h1 class="columns small-12">Gitfolio</h1>
+        <div class="columns small-12">Amazing portfolios, at a click of your fingers</div>
+      </div>
+    </section>
+    <section class="hero__header" :class="{ 'hero__header--initial': !isGithubId }">
+      <h3 class="instruction instruction--lower" :class="{ text: !isGithubId }">Github username</h3>
+      <div class="row">
+        <div class="columns small-12 medium-8 large-6 small-centered username">
+          <div class="username__text" :class="{ text: !isGithubId }">https://github.com/</div>
+          <div class="username__link">
+            <label>
+              <input v-model="githubId" v-on:keyup="delay" class="username__input" type="text" placeholder="li-kai">
+            </label>
+          </div>
+        </div>
+      </div>
+    </section>
+    <transition name="fade">
+    <main v-show="isGithubId">
+      <h3 class="instruction">Pick a theme</h3>
+      <ul class="row small-up-1 medium-up-3 large-up-4 theme-list">
+        <li class="column column-block" v-for="(pic, name, index) in themes">
+          <p-theme-card :github-id="githubId" :theme-name="name" :theme-pic="pic" :theme-id="index"></p-theme-card>
+        </li>
+      </ul>
+    </main>
+    </transition>
   </div>
+</transition>
 </template>
 
 <script>
-import Hello from './components/Hello';
+import throttle from 'lodash.throttle';
+import PThemeCard from './components/ThemeCard';
+import themes from './assets/themes.json';
 
 export default {
   name: 'app',
   components: {
-    Hello,
+    PThemeCard,
+  },
+  methods: {
+    // eslint-disable-next-line
+    delay: throttle(function() {
+      if (Boolean(this.isGithubId) !== Boolean(this.githubId)) {
+        this.isGithubId = this.githubId;
+      }
+    }, 200),
+  },
+  // eslint-disable-next-line
+  data: function() {
+    return {
+      themes,
+      githubId: '',
+      isGithubId: false,
+    };
   },
 };
 </script>
 
 <style lang="scss">
+$body-font-family: 'Raleway', Helvetica, Arial, sans-serif;
+$transition-timing: 0.4s;
 @import '~foundation-sites/scss/foundation';
 @include foundation-everything;
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.hero {
+  height: 100vh;
+  background-size:cover;
+  background-image: url("./img/stock-2.jpg");
+}
+
+.header {
+  padding-top: 1rem;
+}
+
+.hero__header {
+  margin-top: 1rem;
+  transition: transform $transition-timing ease-out, color $transition-timing ease-out;
+}
+
+.hero__header--initial {
+  transform: translateY(20vh);
+}
+
+.instruction {
+  margin-bottom: 0;
+}
+
+.instruction--lower {
+  margin-bottom: -0.7rem;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity $transition-timing ease-out, transform $transition-timing ease-out;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+  transform: translateY(5rem);
+  opacity: 0
+}
+
+.theme-list {
+  list-style: none;
+}
+
+.username {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.username__text {
+  margin-right: 0.2rem;
+  text-align: right;
+}
+
+.text {
+  color: white;
+}
+
+@media (min-width: 768px) {
+  .text {
+    color: inherit;
+  }
+}
+
+.username__input {
+  margin: 0;
 }
 </style>
---
+++ src/components/ThemeCard.vue
@@ -0,0 +1,103 @@
+<template>
+  <a :href="getPortfolio" class="card">
+    <img class="card-img" :src="themePic">
+    <div class="card-section">
+      <h4>{{ themeName }}</h4>
+    </div>
+    <div class="card__color" :class="color">
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'theme-card',
+  props: {
+    githubId: String,
+    themeName: String,
+    themePic: String,
+    themeId: Number,
+  },
+  computed: {
+    // eslint-disable-next-line
+    color: function() {
+      return `card__color--${(this.themeId % 3) + 1}`;
+    },
+    // eslint-disable-next-line
+    getPortfolio: function() {
+      if (this.githubId) {
+        return `pages/${this.githubId}/${this.themeId}`;
+      }
+      return '/';
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.card {
+  display: block;
+  position: relative;
+  transition: opacity 0.35s, transform 0.35s;
+  transform: scale3d(1.05, 1.05, 1);
+  cursor: pointer;
+}
+
+.card:hover {
+  opacity: 0.7;
+  transform: scale3d(1, 1, 1);
+}
+
+.card::before {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(255,255,255,0.5);
+  content: '';
+  transition: transform 0.6s;
+  transform: scale3d(1.9,1.4,1) rotate3d(0,0,1,45deg) translate3d(0,-150%,0);
+}
+
+.card:hover::before {
+  transform: scale3d(1.9,1.4,1) rotate3d(0,0,1,45deg) translate3d(0,100%,0);
+}
+
+.card:hover .card__color {
+  opacity: 0;
+}
+
+.card__color {
+  transition: all 0.325s;
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  opacity: 0.2;
+  height: 100%;
+  width: 100%;
+}
+
+$colors-list: #1b293f, #3C5B8B, #5784CB;
+@for $i from 1 through length($colors-list) {
+  .card__color--#{$i} {
+    background: nth($colors-list, $i);
+  }
+}
+
+.card-img {
+  position: absolute;
+}
+
+.card-section {
+  transition: opacity 0.35s, transform 0.35s;
+  transform: translatey(0.5rem);
+  opacity: 0;
+}
+
+.card-section:hover {
+  opacity: 1;
+  transform: translatey(0);
+}
+</style>
